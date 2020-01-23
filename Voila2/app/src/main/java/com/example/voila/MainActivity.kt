460a8b379b5f82093e977a0dfea68bf0b12d6db4
Mel <54912463+mel-f-dev@users.noreply.github.com>
2020-01-23 16:14:47 +0900

    SplashScreen

---
+++ Voila2/app/src/main/java/com/example/voila/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.voila
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
---
+++ Voila2/app/src/main/java/com/example/voila/SplashScreen.kt
@@ -0,0 +1,29 @@
+package com.example.voila
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.lang.Exception
+import kotlin.concurrent.thread
+
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        
+        val background = object : Thread() {
+            override fun run() {
+                try {
+                    Thread.sleep(1000)
+
+                    val intent = Intent(baseContext, MainActivity::class.java)
+                    startActivity(intent)
+                }catch (e:Exception){
+                    e.printStackTrace()
+                }
+            }
+        }
+        background.start()
+    }
+}
